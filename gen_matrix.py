8a183487875e18ef08b6d001a48d61fee81d7363
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2021-10-26 09:31:43 -0500

    feat: generate test matrices

---
+++ gen_matrix.py
@@ -0,0 +1,16 @@
+#
+# This small helper script generates a test matrix of the given dimension and
+# outputs to stdout so that you can pipe it into files.
+# The elements (in range [1, 100]) are separated by spaces.
+#
+# Synopsis:
+#   ./gen_matrix.py 4 4
+#
+
+import sys
+import random
+
+rows, columns = int(sys.argv[1]), int(sys.argv[2])
+
+for row in range(rows):
+    print(" ".join([str(random.randint(1, 100)) for col in range(columns)]))
